9b2e6aec5dd740188210b903e7dd1db1ac86191e
HunterXdk <mrdk536@gmail.com>
2025-10-05 23:51:48 +0530

    Update: added metadata and phone link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,8 +13,8 @@ export default function App() {
       <Books />
       <h1 className="text-red-600 font-semibold ml-7 mb-1"> GET IN TOUCH </h1>
       <div className="text-blue font-bold ml-7 text-blue-500">Mr. Diwakar Kushwaha </div>
-      <a href="tel:+919584887977" className="text-blue-600 font-bold mx-auto underline hover:text-blue-800">
-       📞 Call Now: +91 9584887977
+      <a href="tel:+91958488" className="text-blue-600 font-bold mx-auto underline hover:text-blue-800">
+       📞 Call Now: +91 958488XXXX
       </a>
 
     </div>
