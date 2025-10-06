15ad7a52b3721e665a23a866f4f5220ddf1f6b0d
HunterXdk <mrdk536@gmail.com>
2025-10-06 07:30:39 +0530

    new update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
       <Books />
       <h1 className="text-red-600 font-semibold ml-7 mb-1"> GET IN TOUCH </h1>
       <div className="text-blue font-bold ml-7 text-blue-500">Mr. Diwakar Kushwaha </div>
-      <a href="tel:+91958488" className="text-blue-600 font-bold mx-auto underline hover:text-blue-800">
+      <a href="tel:+91958488" className="text-blue-600  font-bold mx-auto pb-5 underline hover:text-blue-800">
        📞 Call Now: +91 958488XXXX
       </a>
 
